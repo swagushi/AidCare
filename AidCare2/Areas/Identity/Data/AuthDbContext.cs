1f13c538fe15355caba65376e83d6f7f3adec35e
swagushi <ac111710@avcol.school.nz>
2022-05-11 12:16:16 +1200

    added stuff to shared view

---
+++ AidCare2/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AidCare2.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AidCare2.Data;
+
+public class AuthDbContext : IdentityDbContext<AidCare2User>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- AidCare2/Program.cs
+++ AidCare2/Program.cs
@@ -1,6 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using AidCare2.Data;
+using AidCare2.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuthDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthDbContextConnection' not found.");
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<AidCare2User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AuthDbContext>();;
 
 builder.Services.AddDbContext<AidCare2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AidCare2Context") ?? throw new InvalidOperationException("Connection string 'AidCare2Context' not found.")));
@@ -22,6 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
